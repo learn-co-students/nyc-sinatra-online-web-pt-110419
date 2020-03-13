e618245b6d4aa5240fca0f554ce4874d169c44b8
nicksolony <nicksolony@gmail.com>
2020-03-13 01:22:44 -0400

    Started building up creating a new figure

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,8 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all.sort_by(&:name)
+    erb :'landmarks/index'
+  end
+
 end
